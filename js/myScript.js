db67f2e62a497565715eeea00a44bc97ec5f0c21
Oleshko <olehchoma@gmail.com>
2021-12-26 17:36:34 +0200

    -

--- js/myScript.js
+++ js/myScript.js
@@ -1,9 +1,21 @@
 var date = new Date();
 var time = date.getHours();
-time = 5;
 console.log(time);
 
-// ! bg  change
-var all = document.getElementById('all');
+// ! bg change
+var all = document.querySelector('#all')
 var timeClass = (time > 4 && time < 20) ? 'day' : 'night'
-$(all).addClass(timeClass);
+all.classList.add(timeClass)
+
+var temp = 5;
+var current_temp = document.querySelector('.current_temp')
+current_temp.innerHTML = temp
+
+// invisible space if temperature < 0
+var invSpace = document.querySelector('.inv-space')
+if (temp>=0) {
+    invSpace.classList.remove('none')
+}
+else {
+    invSpace.classList.add('none')
+}
