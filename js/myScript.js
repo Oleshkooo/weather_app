028af23a9dd8645a38cd706bffcebb3a2cd3740b
Oleshko <olehchoma@gmail.com>
2021-12-27 20:17:47 +0200

    -

--- js/myScript.js
+++ js/myScript.js
@@ -1,42 +1,80 @@
 // ! constants
-const API          = "97097b6bdad34048ed5f17c1097cb86a"
-var   latitude     = 49.8383
-var   longitude    = 24.0232
+const API       = "97097b6bdad34048ed5f17c1097cb86a"
+let   latitude  = 0;
+let   longitude = 0;
 
-var   currentCity  = 'Lviv'
-var   current_city = document.querySelector('.current_city')
-
-
-// === current ===
-// weather
-var currentTemp
-var currentDescription
-var currentTempMax
-var currentTempMin
-// html
-var current_temp        = document.querySelector('.current_temp')
-var current_description = document.querySelector('.current_description')
-var current_temp_max    = document.querySelector('.current_temp_max')
-var current_temp_min    = document.querySelector('.current_temp_min')
-// default html changes
-
-
-// ! window onload
-document.addEventListener('load', event => {
-    // / coords
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            latitude = position.coords.latitude
-            longitude = position.coords.longitude
-            // console.log(latitude, longitude)
-            currentCity  = 'Lviv'
-            current_city.innerHTML = currentCity
-            console.log("onload")
-        })
-    }
-    main();
-})
-
-function main() {
-    console.log("asd")
+const current = {
+    city       : 'City',
+    description: 'Description',
+    temp       : 0,
+    maxTemp    : 0,
+    minTemp    : 0,
 }
+
+const text = {
+    city       : document.querySelector('.current_city'),
+    description: document.querySelector('.current_description'),
+    temp       : document.querySelector('.current_temp'),
+    maxTemp    : document.querySelector('.current_temp_max'),
+    minTemp    : document.querySelector('.current_temp_min'),
+}
+
+// === === === === ===
+
+// / start funcrions
+changeBg();
+updateHTML();
+
+
+// === === === === ===
+
+// / coords
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+        latitude  = position.coords.latitude
+        longitude = position.coords.longitude
+        // console.log(latitude, longitude)
+    })
+    current.city = 'Lviv'
+}
+
+
+
+// / change background
+function changeBg() {
+    var date      = new Date();
+    var time      = date.getHours();
+    var all       = document.querySelector('#all')
+    var timeClass = (time > 4 && time < 20) ? 'day' : 'night'
+    all.classList.add(timeClass)
+}
+
+// / get weather
+function getWeather() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${current.city}&appid=${API}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            // / weather
+            current.description = data.weather[0].main
+            current.temp        = Math.round(data.main.temp)
+            current.maxTemp     = Math.ceil(data.main.temp_max)
+            current.minTemp     = Math.floor(data.main.temp_min)
+            updateHTML();
+        })
+        .catch(err => console.log("[ERROR] Wrong city name"))
+}
+
+// / update HTML
+function updateHTML() {
+    text.city.innerHTML        = current.city
+    text.description.innerHTML = current.description
+    text.temp.innerHTML        = current.temp
+    text.city.innerHTML        = current.city
+    text.city.innerHTML        = current.city
+}
+
+// / endless reload
+setInterval(() => {
+    changeBg();
+    getWeather();
+}, 1000);
